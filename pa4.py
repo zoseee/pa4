0c64a1a172f6d186bf3bc57fc811d27ca654536e
Jakob <jtigger7010@gmail.com>
2023-12-07 20:53:40 -0800

    fifo

--- pa4.py
+++ pa4.py
@@ -1,4 +1,8 @@
 import random
+from collections import deque  # Import deque for FIFO
+
+
+random.seed(42)
 
 class PageTableEntry:
     def __init__(self):
@@ -22,6 +26,55 @@ class Memory:
         self.page_faults = 0
         self.disk_references = 0
         self.dirty_writes = 0
+        self.page_order = deque()  # Initialize the deque for FIFO
+
+def fifo_algo(processed_data):
+    data = "" 
+    page_faults = 0
+    disk_accesses = 0
+    dirty_page_writes = 0
+
+    # data structures
+    main_memory = set()
+    page_queue = []
+    page_table = {}
+
+    for access in processed_data:
+        process_number, memory_reference, read_or_write = access
+
+        # calculate virtual page # and offset
+        virtual_page_number = memory_reference >> 0
+        offset = memory_reference & 0x1FF
+
+        # check if page is in main memory
+        if virtual_page_number not in main_memory:
+            page_faults += 1
+
+            # check if main memory is full
+            if len(main_memory) == addressable_physical_pages:
+                oldest_page = page_queue.pop(0)
+                main_memory.remove(oldest_page)
+
+                #check if oldest page was dirty
+                if page_table[oldest_page]['dirty']:
+                    disk_accesses += 2
+                    dirty_page_writes += 1
+                else:
+                    disk_accesses += 1
+
+            # load the new page in the main memory
+            main_memory.add(virtual_page_number)
+            page_queue.append(virtual_page_number)
+
+            #update page table enttry
+            page_table[virtual_page_number] = {'dirty': False, 'reference': True}
+
+        #check if memory access is write
+        if read_or_write == 'W':
+            page_table[virtual_page_number]['dirty'] = True
+
+    data = f"Total Page Faults: {page_faults}\nTotal Disk References: {disk_accesses}\nTotal Dirty Page Writes: {dirty_page_writes}"
+    return data
 
 def simulate_page_replacement(memory, processes, algorithm):
     max_time_units = max(len(process.memory_references) for process in processes)
@@ -33,7 +86,7 @@ def simulate_page_replacement(memory, processes, algorithm):
                 virtual_address = int(virtual_address)  # Convert to integer here
 
                 
-                virtual_page_number = (virtual_address >> 9) & 0b11111  # Use 0b11111 to get the 5 least significant bits
+                virtual_page_number = (virtual_address >> 9) % 32
 
 
                 offset = virtual_address % 512
@@ -43,7 +96,7 @@ def simulate_page_replacement(memory, processes, algorithm):
                     memory.page_faults += 1
 
                     if page_table_entry.dirty:
-                        memory.disk_references += 2
+                        memory.disk_references += 1
                         memory.dirty_writes += 1
                         # Write the dirty page back to disk
 
@@ -62,14 +115,45 @@ def simulate_page_replacement(memory, processes, algorithm):
                 if operation == 'W':
                     page_table_entry.dirty = True
 
-                # Simulate the page replacement algorithm
+                
                 if algorithm == 'RAND':
                     victim_page = random.randint(0, 31)
-                    # Replace the victim page in memory
-                    memory.pages[victim_page] = process.page_table.entries[virtual_page_number]
+                    victim_page_entry = memory.pages[victim_page]
+                    
+                    if victim_page_entry.dirty:
+                        memory.disk_references += 1
+                        memory.dirty_writes += 1
+                        # Write the dirty page back to disk
+
                     # Update the corresponding page table entry
-                    process.page_table.entries[virtual_page_number] = memory.pages[victim_page]
-                    # Perform any necessary disk operations
+                    process.page_table.entries[virtual_page_number] = victim_page_entry
+                    victim_page_entry.valid = True
+                    victim_page_entry.dirty = False
+                    victim_page_entry.referenced = True
+                    # Load the new page from disk
+
+                elif algorithm == 'FIFO':
+                    if memory.page_order:    
+
+                        oldest_page = memory.page_order.popleft()
+                        oldest_page_entry = memory.pages[oldest_page]
+
+                        if oldest_page_entry.dirty:
+                            memory.disk_references += 1
+                            memory.dirty_writes += 1
+                            # Write the dirty page back to disk
+
+                        # Update the corresponding page table entry
+                        process.page_table.entries[virtual_page_number] = oldest_page_entry
+                        oldest_page_entry.valid = True
+                        oldest_page_entry.dirty = False
+                        oldest_page_entry.referenced = True
+                        # Load the new page from disk
+
+                    # Update the page order for both algorithms
+                memory.page_order.append(virtual_page_number)
+
+
 
 
 
@@ -104,7 +188,7 @@ if __name__ == "__main__":
         memory = Memory()
 
         # Simulate page replacement with a specific algorithm
-        simulate_page_replacement(memory, process_objects, algorithm='RAND')
+        simulate_page_replacement(memory, process_objects, algorithm='FIFO')
 
         # Print the simulation results
         print(f"Total Page Faults: {memory.page_faults}")
